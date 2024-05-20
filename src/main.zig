7dd7c6148dd2b8dec9d8e0932d2081292812778e
-k <slowdive@me.com>
2024-05-20 09:22:44 -0700

    feat: implement `rand`

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,20 @@ const std = @import("std");
 const Attr = tb.AttributeSet;
 
 fn printCell(width: i32, height: i32) !void {
+    var rand = std.rand.DefaultPrng.init(@as(u64, @bitCast(std.time.milliTimestamp())));
+
     for (0..@intCast(width)) |w| {
         for (0..@intCast(height)) |h| {
-            _ = try tb.print(@intCast(w), @intCast(h), Attr.init(.red).add(.bold), Attr.init(.default), "X");
-            try tb.present();
+            const number = @mod(rand.random().int(i8), 2);
+            const int: u8 = @intCast(number);
+
+            var buf: [2]u8 = undefined;
+            var slice: [:0]u8 = try std.fmt.bufPrintZ(&buf, "{d}", .{int});
+
+            _ = try tb.print(@intCast(w), @intCast(h), Attr.init(.red).add(.bold), Attr.init(.default), slice);
         }
+
+        try tb.present();
     }
 }
 
@@ -20,7 +29,6 @@ pub fn main() !void {
 
     try printCell(width, height);
 
-    std.time.sleep(1000000000);
-
+    _ = try tb.pollEvent();
     try tb.shutdown();
 }
