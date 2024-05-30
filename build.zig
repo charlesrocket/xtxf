f95f7cecdfbed7f3c01ebf77188a09ecd1369f96
-k <slowdive@me.com>
2024-05-30 09:15:10 -0700

    test: add basic test

--- build.zig
+++ build.zig
@@ -28,13 +28,9 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    const test_ok = b.addRunArtifact(exe);
+    test_ok.addArgs(&.{"-t=short"});
+    test_ok.expectExitCode(0);
+    test_step.dependOn(&test_ok.step);
 }
