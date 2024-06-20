f556cb0e25b9ca793eae46ee9ee7a1309b2d7a5e
-k <slowdive@me.com>
2024-06-20 08:34:21 -0700

    test: add `columns`

--- tests/cli.zig
+++ tests/cli.zig
@@ -1,9 +1,25 @@
 const std = @import("std");
 const build_options = @import("build_options");
 
-test "main" {
+test "default" {
     const exe_path = build_options.exe_path;
     const argv = [_][]const u8{ exe_path, "--time=short" };
+    const proc = try std.process.Child.run(.{
+        .allocator = std.testing.allocator,
+        .argv = &argv,
+    });
+
+    defer std.testing.allocator.free(proc.stdout);
+    defer std.testing.allocator.free(proc.stderr);
+
+    const term = proc.term;
+
+    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+}
+
+test "columns" {
+    const exe_path = build_options.exe_path;
+    const argv = [_][]const u8{ exe_path, "--time=short", "-s=columns" };
     const proc = try std.process.Child.run(.{
         .allocator = std.testing.allocator,
         .argv = &argv,
