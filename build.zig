e224f263fb8916b02b5b6b1cc8fcab89216a5a5d
-k <slowdive@me.com>
2024-06-20 11:39:31 -0700

    ops: fix kcov integration

--- build.zig
+++ build.zig
@@ -55,35 +55,16 @@ pub fn build(b: *std.Build) void {
 
     test_step.dependOn(&b.addRunArtifact(integration_tests).step);
 
+    const merge_step = b.addSystemCommand(&.{ "kcov", "--merge", "kcov-out", "kcov-test", "kcov-unit" });
+
+    const kcov_int = b.addSystemCommand(&.{ "kcov", "kcov-test", "--include-path=src" });
+    kcov_int.addArtifactArg(integration_tests);
+    merge_step.step.dependOn(&kcov_int.step);
+
+    const kcov_unit = b.addSystemCommand(&.{ "kcov", "kcov-unit", "--include-path=src" });
+    kcov_unit.addArtifactArg(unit_tests);
+    merge_step.step.dependOn(&kcov_unit.step);
+
     const coverage_step = b.step("coverage", "Generate test coverage (kcov)");
-    const merge_step = std.Build.Step.Run.create(b, "merge coverage");
-    merge_step.addArgs(&.{ "kcov", "--merge" });
-    merge_step.rename_step_with_output_arg = false;
-    const merged_coverage_output = merge_step.addOutputFileArg(".");
-
-    {
-        const kcov_collect = std.Build.Step.Run.create(b, "collect coverage");
-        kcov_collect.addArgs(&.{ "kcov", "--collect-only" });
-        kcov_collect.addPrefixedDirectoryArg("--include-pattern=", b.path("src"));
-        merge_step.addDirectoryArg(kcov_collect.addOutputFileArg(unit_tests.name));
-        kcov_collect.addArtifactArg(unit_tests);
-        kcov_collect.enableTestRunnerMode();
-    }
-
-    {
-        const kcov_collect = std.Build.Step.Run.create(b, "collect coverage");
-        kcov_collect.addArgs(&.{ "kcov", "--collect-only" });
-        kcov_collect.addPrefixedDirectoryArg("--include-pattern=", b.path("src"));
-        merge_step.addDirectoryArg(kcov_collect.addOutputFileArg(integration_tests.name));
-        kcov_collect.addArtifactArg(integration_tests);
-        kcov_collect.enableTestRunnerMode();
-    }
-
-    const install_coverage = b.addInstallDirectory(.{
-        .source_dir = merged_coverage_output,
-        .install_dir = .{ .custom = "coverage" },
-        .install_subdir = "",
-    });
-
-    coverage_step.dependOn(&install_coverage.step);
+    coverage_step.dependOn(&merge_step.step);
 }
