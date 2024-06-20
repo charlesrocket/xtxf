470ac335c69191af0eb58806279a847d74de1db5
-k <slowdive@me.com>
2024-06-20 08:34:33 -0700

    test: add `crypto`

--- tests/cli.zig
+++ tests/cli.zig
@@ -32,3 +32,19 @@ test "columns" {
 
     try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
 }
+
+test "crypto" {
+    const exe_path = build_options.exe_path;
+    const argv = [_][]const u8{ exe_path, "--time=short", "-s=crypto" };
+    const proc = try std.process.Child.run(.{
+        .allocator = std.testing.allocator,
+        .argv = &argv,
+    });
+
+    defer std.testing.allocator.free(proc.stdout);
+    defer std.testing.allocator.free(proc.stderr);
+
+    const term = proc.term;
+
+    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+}
