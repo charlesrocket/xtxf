f4b9c918c18552a63670f6c882ba765628328600
-k <slowdive@me.com>
2024-08-17 08:40:50 -0700

    fix: reformat help message

--- README.md
+++ README.md
@@ -24,11 +24,11 @@ Usage: xtxf [OPTIONS]
 Example: xtxf -p -c=red -s=crypto
 
 Options:
-  -c, --color     Set color [default, red, green, blue, yellow, magenta]
-  -s, --style     Set style [default, columns, crypto, grid, blocks]
-  -t, --time      Set duration [loop, short]
-  -p, --pulse     Pulse blocks
-  -d, --decimal   Decimal mode
-  -v, --version   Print version
-  -h, --help      Print this message
+  -c, --color    Set color [default, red, green, blue, yellow, magenta]
+  -s, --style    Set style [default, columns, crypto, grid, blocks]
+  -t, --time     Set duration [loop, short]
+  -p, --pulse    Pulse blocks
+  -d, --decimal  Decimal mode
+  -v, --version  Print version
+  -h, --help     Print this message
 ```
--- src/main.zig
+++ src/main.zig
@@ -316,13 +316,13 @@ pub fn main() !void {
         \\Example: xtxf -p -c=red -s=crypto
         \\
         \\Options:
-        \\  -c, --color     Set color [default, red, green, blue, yellow, magenta]
-        \\  -s, --style     Set style [default, columns, crypto, grid, blocks]
-        \\  -t, --time      Set duration [loop, short]
-        \\  -p, --pulse     Pulse blocks
-        \\  -d, --decimal   Decimal mode
-        \\  -v, --version   Print version
-        \\  -h, --help      Print this message
+        \\  -c, --color    Set color [default, red, green, blue, yellow, magenta]
+        \\  -s, --style    Set style [default, columns, crypto, grid, blocks]
+        \\  -t, --time     Set duration [loop, short]
+        \\  -p, --pulse    Pulse blocks
+        \\  -d, --decimal  Decimal mode
+        \\  -v, --version  Print version
+        \\  -h, --help     Print this message
     ;
 
     for (args) |arg| {
