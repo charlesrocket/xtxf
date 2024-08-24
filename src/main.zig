6e1098b1939ac19170497d53c37bb6979d50c467
-k <slowdive@me.com>
2024-08-24 00:23:02 -0700

    feat: add custom usage function

--- src/main.zig
+++ src/main.zig
@@ -27,6 +27,55 @@ pub const CommandT = cova.Command.Custom(.{
     .global_help_prefix = "xtxf-" ++ build_opt.head_hash[0..7],
     .help_header_fmt = assets.help_message,
     .examples_header_fmt = assets.examples_header,
+    .global_usage_fn = struct {
+        fn usage(self: anytype, writer: anytype, _: std.mem.Allocator) !void {
+            const CmdT = @TypeOf(self.*);
+            const OptT = CmdT.OptionT;
+            const indent_fmt = CmdT.indent_fmt;
+            var no_args = true;
+            var pre_sep: []const u8 = "";
+
+            try writer.print("USAGE:\n", .{});
+            if (self.opts) |opts| {
+                no_args = false;
+                try writer.print("{s}{s} ", .{
+                    indent_fmt,
+                    self.name,
+                });
+                for (opts) |opt| {
+                    try writer.print("{s} {s}{s} <{s}>", .{
+                        pre_sep,
+                        OptT.long_prefix orelse opt.short_prefix,
+                        opt.long_name orelse &.{opt.short_name orelse 0},
+                        opt.val.name(),
+                    });
+                    pre_sep = "\n " ++ indent_fmt ++ indent_fmt;
+                }
+                try writer.print("\n\n", .{});
+            }
+            if (self.sub_cmds) |cmds| {
+                no_args = false;
+                try writer.print("{s}{s} [", .{
+                    indent_fmt,
+                    self.name,
+                });
+                pre_sep = "";
+                for (cmds) |cmd| {
+                    try writer.print("{s} {s} ", .{
+                        pre_sep,
+                        cmd.name,
+                    });
+                    pre_sep = "|";
+                }
+                try writer.print("]\n\n", .{});
+            }
+            if (no_args) try writer.print("{s}{s}{s}", .{
+                indent_fmt,
+                indent_fmt,
+                self.name,
+            });
+        }
+    }.usage,
     .opt_config = .{
         .usage_fmt = assets.opt_usage,
     },
