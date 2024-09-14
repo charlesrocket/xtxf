c8328f6fcf5af501f66acb3bf6bac6d1020aeacd
-k <slowdive@me.com>
2024-09-14 11:01:05 -0700

    test: improve `runner()`

--- test/cli.zig
+++ test/cli.zig
@@ -4,7 +4,13 @@ const build_options = @import("build_options");
 const exe_path = build_options.exe_path;
 const live = build_options.test_live;
 
-fn runner(args: anytype) !std.process.Child.Term {
+const Proc = struct {
+    term: std.process.Child.Term,
+    out: []u8,
+    err: []u8,
+};
+
+fn runner(args: [4][]const u8) !Proc {
     var proc = std.process.Child.init(&args, std.testing.allocator);
 
     proc.stdout_behavior = .Pipe;
@@ -22,110 +28,118 @@ fn runner(args: anytype) !std.process.Child.Term {
 
     const term = try proc.wait();
 
-    return term;
+    const out = try stdout.toOwnedSlice();
+    const err = try stderr.toOwnedSlice();
+
+    defer {
+        std.testing.allocator.free(out);
+        std.testing.allocator.free(err);
+    }
+
+    return Proc{ .term = term, .out = out, .err = err };
 }
 
 test "default" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=default", "-c=default" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=default", "-c=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "speed" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=default", "--speed=slow" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=default", "--speed=slow" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "mode: decimal" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-m=decimal" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-m=decimal", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "mode: hexadecimal" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-m=hexadecimal" };
-    const term = try runner(argv);
+    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-m=hexadecimal", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "mode: textual" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-m=textual" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-m=textual", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "color: red" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=red" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=red", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "color: green" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=green" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=green", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "color: blue" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=blue" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=blue", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "color: yellow" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=yellow" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=yellow", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "color: magenta" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=magenta" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-c=magenta", "-s=default" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "style: columns" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=columns" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=columns", "-m=decimal" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "style: crypto" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=crypto" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=crypto", "-m=hexadecimal" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "style: grid" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=grid" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=grid", "-m=binary" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "style: blocks" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=blocks" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=blocks", "-m=decimal" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
 test "style: rain" {
-    const argv = [_][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=rain" };
-    const term = try runner(argv);
+    const argv = [4][]const u8{ exe_path, if (live) "--time=1" else "--debug", "-s=rain", "-m=textual" };
+    const proc = try runner(argv);
 
-    try std.testing.expectEqual(term, std.process.Child.Term{ .Exited = 0 });
+    try std.testing.expectEqual(proc.term.Exited, 0);
 }
