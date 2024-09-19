1e6fb2510b6dc80aef773c233506efe9347d25d7
-k <slowdive@me.com>
2024-09-19 01:15:45 -0700

    test: move err streams

--- test/cli.zig
+++ test/cli.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const streams = @import("streams.zig");
 
 const build_options = @import("build_options");
 const exe_path = build_options.exe_path;
@@ -41,7 +42,7 @@ test "default" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, default_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.default);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -53,7 +54,7 @@ test "speed" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, default_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.default);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -65,7 +66,7 @@ test "mode: decimal" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, decimal_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.decimal);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -77,7 +78,7 @@ test "mode: hexadecimal" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, hexadecimal_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.hexadecimal);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -89,7 +90,7 @@ test "mode: textual" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, textual_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.textual);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -101,7 +102,7 @@ test "color: red" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, red_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.red);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -113,7 +114,7 @@ test "color: green" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, green_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.green);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -124,7 +125,7 @@ test "color: blue" {
         std.testing.allocator.free(proc.out);
         std.testing.allocator.free(proc.err);
     }
-    try std.testing.expectStringEndsWith(proc.err, blue_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.blue);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -136,7 +137,7 @@ test "color: yellow" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, yellow_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.yellow);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -148,7 +149,7 @@ test "color: magenta" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, magenta_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.magenta);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -160,7 +161,7 @@ test "style: columns" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, columns_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.columns);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -172,7 +173,7 @@ test "style: crypto" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, crypto_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.crypto);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -184,7 +185,7 @@ test "style: grid" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, grid_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.grid);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -196,7 +197,7 @@ test "style: blocks" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, blocks_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.blocks);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
 
@@ -208,187 +209,6 @@ test "style: rain" {
         std.testing.allocator.free(proc.err);
     }
 
-    try std.testing.expectStringEndsWith(proc.err, rain_run);
+    try std.testing.expectStringEndsWith(proc.err, streams.rain);
     try std.testing.expectEqual(proc.term.Exited, 0);
 }
-
-const default_run =
-    \\info(xtxf): 0: 19x0 0/0
-    \\info(xtxf): 0: 19x1 0/0
-    \\info(xtxf): 0: 19x2 256/0
-    \\info(xtxf): 0: 19x3 256/0
-    \\info(xtxf): 0: 19x4 256/0
-    \\info(xtxf): 1: 19x5 256/0
-    \\info(xtxf): 0: 19x6 256/0
-    \\info(xtxf): 1: 19x7 0/0
-    \\info(xtxf): 0: 19x8 256/0
-    \\info(xtxf): 0: 19x9 0/0
-    \\info(xtxf): 1: 19x10 256/0
-    \\info(xtxf): 0: 19x11 256/0
-    \\info(xtxf): 1: 19x12 0/0
-    \\info(xtxf): 1: 19x13 256/0
-    \\info(xtxf): 1: 19x14 0/0
-    \\info(xtxf): 1: 19x15 0/0
-    \\info(xtxf): 1: 19x16 0/0
-    \\info(xtxf): 0: 19x17 0/0
-    \\info(xtxf): 1: 19x18 0/0
-    \\info(xtxf): 1: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const decimal_run =
-    \\info(xtxf): 9: 19x10 0/0
-    \\info(xtxf): 8: 19x11 0/0
-    \\info(xtxf): 2: 19x12 256/0
-    \\info(xtxf): 2: 19x13 256/0
-    \\info(xtxf): 9: 19x14 256/0
-    \\info(xtxf): 6: 19x15 0/0
-    \\info(xtxf): 6: 19x16 0/0
-    \\info(xtxf): 5: 19x17 256/0
-    \\info(xtxf): 0: 19x18 256/0
-    \\info(xtxf): 0: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const hexadecimal_run =
-    \\info(xtxf): F: 19x12 0/0
-    \\info(xtxf): D: 19x13 256/0
-    \\info(xtxf): 3: 19x14 0/0
-    \\info(xtxf): 7: 19x15 0/0
-    \\info(xtxf): D: 19x16 0/0
-    \\info(xtxf): 4: 19x17 0/0
-    \\info(xtxf): 3: 19x18 0/0
-    \\info(xtxf): 9: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const textual_run =
-    \\info(xtxf): +: 19x12 0/0
-    \\info(xtxf): -: 19x13 0/0
-    \\info(xtxf): ｹ: 19x14 0/0
-    \\info(xtxf): ﾏ: 19x15 256/0
-    \\info(xtxf): V: 19x16 0/0
-    \\info(xtxf): ﾅ: 19x17 0/0
-    \\info(xtxf): ﾄ: 19x18 0/0
-    \\info(xtxf): ｢: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const red_run =
-    \\info(xtxf): 1: 19x13 258/0
-    \\info(xtxf): 1: 19x14 2/0
-    \\info(xtxf): 1: 19x15 2/0
-    \\info(xtxf): 1: 19x16 2/0
-    \\info(xtxf): 0: 19x17 2/0
-    \\info(xtxf): 1: 19x18 2/0
-    \\info(xtxf): 1: 19x19 2/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const green_run =
-    \\info(xtxf): 1: 19x13 259/0
-    \\info(xtxf): 1: 19x14 3/0
-    \\info(xtxf): 1: 19x15 3/0
-    \\info(xtxf): 1: 19x16 3/0
-    \\info(xtxf): 0: 19x17 3/0
-    \\info(xtxf): 1: 19x18 3/0
-    \\info(xtxf): 1: 19x19 3/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const blue_run =
-    \\info(xtxf): 1: 19x13 261/0
-    \\info(xtxf): 1: 19x14 5/0
-    \\info(xtxf): 1: 19x15 5/0
-    \\info(xtxf): 1: 19x16 5/0
-    \\info(xtxf): 0: 19x17 5/0
-    \\info(xtxf): 1: 19x18 5/0
-    \\info(xtxf): 1: 19x19 5/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const yellow_run =
-    \\info(xtxf): 1: 19x13 260/0
-    \\info(xtxf): 1: 19x14 4/0
-    \\info(xtxf): 1: 19x15 4/0
-    \\info(xtxf): 1: 19x16 4/0
-    \\info(xtxf): 0: 19x17 4/0
-    \\info(xtxf): 1: 19x18 4/0
-    \\info(xtxf): 1: 19x19 4/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const magenta_run =
-    \\info(xtxf): 1: 19x13 262/0
-    \\info(xtxf): 1: 19x14 6/0
-    \\info(xtxf): 1: 19x15 6/0
-    \\info(xtxf): 1: 19x16 6/0
-    \\info(xtxf): 0: 19x17 6/0
-    \\info(xtxf): 1: 19x18 6/0
-    \\info(xtxf): 1: 19x19 6/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const columns_run =
-    \\info(xtxf): 6: 19x14 0/0
-    \\info(xtxf): 4: 19x15 0/0
-    \\info(xtxf): 3: 19x16 256/0
-    \\info(xtxf): 4: 19x17 256/0
-    \\info(xtxf): 2: 19x18 0/0
-    \\info(xtxf): 3: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const crypto_run =
-    \\info(xtxf): 1: 19x11 256/0
-    \\info(xtxf): 1: 19x13 0/0
-    \\info(xtxf): 3: 19x14 256/0
-    \\info(xtxf): E: 19x16 0/0
-    \\info(xtxf): 9: 19x17 0/0
-    \\info(xtxf): 8: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const grid_run =
-    \\info(xtxf): 0: 19x9 256/0
-    \\info(xtxf): 1: 19x11 0/0
-    \\info(xtxf): 0: 19x13 0/0
-    \\info(xtxf): 0: 19x15 0/0
-    \\info(xtxf): 0: 19x17 256/0
-    \\info(xtxf): 0: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const blocks_run =
-    \\info(xtxf): 1: 19x13 0/0
-    \\info(xtxf): 2: 19x14 0/0
-    \\info(xtxf): 7: 19x15 0/0
-    \\info(xtxf): 4: 19x16 256/0
-    \\info(xtxf): 5: 19x17 0/0
-    \\info(xtxf): 4: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
-
-const rain_run =
-    \\info(xtxf): ﾄ: 19x14 0/0
-    \\info(xtxf): ｾ: 19x15 0/0
-    \\info(xtxf): ｰ: 19x16 256/0
-    \\info(xtxf): ﾆ: 19x17 0/0
-    \\info(xtxf): #: 19x18 0/0
-    \\info(xtxf): 6: 19x19 0/0
-    \\info(xtxf): Exiting...
-    \\
-;
---
+++ test/streams.zig
@@ -0,0 +1,180 @@
+pub const default =
+    \\info(xtxf): 0: 19x0 0/0
+    \\info(xtxf): 0: 19x1 0/0
+    \\info(xtxf): 0: 19x2 256/0
+    \\info(xtxf): 0: 19x3 256/0
+    \\info(xtxf): 0: 19x4 256/0
+    \\info(xtxf): 1: 19x5 256/0
+    \\info(xtxf): 0: 19x6 256/0
+    \\info(xtxf): 1: 19x7 0/0
+    \\info(xtxf): 0: 19x8 256/0
+    \\info(xtxf): 0: 19x9 0/0
+    \\info(xtxf): 1: 19x10 256/0
+    \\info(xtxf): 0: 19x11 256/0
+    \\info(xtxf): 1: 19x12 0/0
+    \\info(xtxf): 1: 19x13 256/0
+    \\info(xtxf): 1: 19x14 0/0
+    \\info(xtxf): 1: 19x15 0/0
+    \\info(xtxf): 1: 19x16 0/0
+    \\info(xtxf): 0: 19x17 0/0
+    \\info(xtxf): 1: 19x18 0/0
+    \\info(xtxf): 1: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const decimal =
+    \\info(xtxf): 9: 19x10 0/0
+    \\info(xtxf): 8: 19x11 0/0
+    \\info(xtxf): 2: 19x12 256/0
+    \\info(xtxf): 2: 19x13 256/0
+    \\info(xtxf): 9: 19x14 256/0
+    \\info(xtxf): 6: 19x15 0/0
+    \\info(xtxf): 6: 19x16 0/0
+    \\info(xtxf): 5: 19x17 256/0
+    \\info(xtxf): 0: 19x18 256/0
+    \\info(xtxf): 0: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const hexadecimal =
+    \\info(xtxf): F: 19x12 0/0
+    \\info(xtxf): D: 19x13 256/0
+    \\info(xtxf): 3: 19x14 0/0
+    \\info(xtxf): 7: 19x15 0/0
+    \\info(xtxf): D: 19x16 0/0
+    \\info(xtxf): 4: 19x17 0/0
+    \\info(xtxf): 3: 19x18 0/0
+    \\info(xtxf): 9: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const textual =
+    \\info(xtxf): +: 19x12 0/0
+    \\info(xtxf): -: 19x13 0/0
+    \\info(xtxf): ｹ: 19x14 0/0
+    \\info(xtxf): ﾏ: 19x15 256/0
+    \\info(xtxf): V: 19x16 0/0
+    \\info(xtxf): ﾅ: 19x17 0/0
+    \\info(xtxf): ﾄ: 19x18 0/0
+    \\info(xtxf): ｢: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const red =
+    \\info(xtxf): 1: 19x13 258/0
+    \\info(xtxf): 1: 19x14 2/0
+    \\info(xtxf): 1: 19x15 2/0
+    \\info(xtxf): 1: 19x16 2/0
+    \\info(xtxf): 0: 19x17 2/0
+    \\info(xtxf): 1: 19x18 2/0
+    \\info(xtxf): 1: 19x19 2/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const green =
+    \\info(xtxf): 1: 19x13 259/0
+    \\info(xtxf): 1: 19x14 3/0
+    \\info(xtxf): 1: 19x15 3/0
+    \\info(xtxf): 1: 19x16 3/0
+    \\info(xtxf): 0: 19x17 3/0
+    \\info(xtxf): 1: 19x18 3/0
+    \\info(xtxf): 1: 19x19 3/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const blue =
+    \\info(xtxf): 1: 19x13 261/0
+    \\info(xtxf): 1: 19x14 5/0
+    \\info(xtxf): 1: 19x15 5/0
+    \\info(xtxf): 1: 19x16 5/0
+    \\info(xtxf): 0: 19x17 5/0
+    \\info(xtxf): 1: 19x18 5/0
+    \\info(xtxf): 1: 19x19 5/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const yellow =
+    \\info(xtxf): 1: 19x13 260/0
+    \\info(xtxf): 1: 19x14 4/0
+    \\info(xtxf): 1: 19x15 4/0
+    \\info(xtxf): 1: 19x16 4/0
+    \\info(xtxf): 0: 19x17 4/0
+    \\info(xtxf): 1: 19x18 4/0
+    \\info(xtxf): 1: 19x19 4/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const magenta =
+    \\info(xtxf): 1: 19x13 262/0
+    \\info(xtxf): 1: 19x14 6/0
+    \\info(xtxf): 1: 19x15 6/0
+    \\info(xtxf): 1: 19x16 6/0
+    \\info(xtxf): 0: 19x17 6/0
+    \\info(xtxf): 1: 19x18 6/0
+    \\info(xtxf): 1: 19x19 6/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const columns =
+    \\info(xtxf): 6: 19x14 0/0
+    \\info(xtxf): 4: 19x15 0/0
+    \\info(xtxf): 3: 19x16 256/0
+    \\info(xtxf): 4: 19x17 256/0
+    \\info(xtxf): 2: 19x18 0/0
+    \\info(xtxf): 3: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const crypto =
+    \\info(xtxf): 1: 19x11 256/0
+    \\info(xtxf): 1: 19x13 0/0
+    \\info(xtxf): 3: 19x14 256/0
+    \\info(xtxf): E: 19x16 0/0
+    \\info(xtxf): 9: 19x17 0/0
+    \\info(xtxf): 8: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const grid =
+    \\info(xtxf): 0: 19x9 256/0
+    \\info(xtxf): 1: 19x11 0/0
+    \\info(xtxf): 0: 19x13 0/0
+    \\info(xtxf): 0: 19x15 0/0
+    \\info(xtxf): 0: 19x17 256/0
+    \\info(xtxf): 0: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const blocks =
+    \\info(xtxf): 1: 19x13 0/0
+    \\info(xtxf): 2: 19x14 0/0
+    \\info(xtxf): 7: 19x15 0/0
+    \\info(xtxf): 4: 19x16 256/0
+    \\info(xtxf): 5: 19x17 0/0
+    \\info(xtxf): 4: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
+
+pub const rain =
+    \\info(xtxf): ﾄ: 19x14 0/0
+    \\info(xtxf): ｾ: 19x15 0/0
+    \\info(xtxf): ｰ: 19x16 256/0
+    \\info(xtxf): ﾆ: 19x17 0/0
+    \\info(xtxf): #: 19x18 0/0
+    \\info(xtxf): 6: 19x19 0/0
+    \\info(xtxf): Exiting...
+    \\
+;
